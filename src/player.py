fea8ba30d0cf0f3ed766215fd737ab26e5e44359
Grazia Ferrara <g.ferrara75@studenti.unisa.it>
2023-06-09 18:18:26 +0200

    inizio comunicazione per la generazione di stringhe

--- src/player.py
+++ src/player.py
@@ -11,6 +11,8 @@ class Player(User):
     This class represents a player of the bingo game, it inherits from the User class.
     """
     
+    # AUTHENTICATION
+    
     def send_GP(self):
         """ 
         Send the GP certificate to the sala bingo.
@@ -62,3 +64,67 @@ class Player(User):
         # return the fields values whose key is in the policy
         proofs, indices = self.__compute_proofs(policy)
         return {key: self.clear_fields[key] for key in policy}, proofs, indices
+    
+    # GAME
+    
+    def send_commitment(self):
+        """
+        The player computes its own commitment, and computes the signature
+        on the committment. Then he sends them and the game parameters
+        (timestamp, player_id and game_id).
+        # Returns
+            params: tuple
+                The game parameters.
+            commitment: string
+                The commitment.
+            signature: string
+                The signature of the player on the commitment.
+        """
+        return self._params, self.__compute_commitment(), self.__sign_message()
+    
+    def recieve_signature(self, signature):
+        """ 
+        The player receives the signature of the sala bingo on the commitment,
+        the signature and the additional parameters. Then it verifies the 
+        signature.
+        # Returns
+            true if the signature is valid, false otherwise.
+        """
+        pass
+    
+    def recieve_commitments_and_signature(self, values, signature):
+        """
+        The player receives the commitments, the parameters and the 
+        signature of the sala bingo on them. Then it verifies the 
+        signature.
+        
+        # Arguments
+            values: list
+                The list of values.
+            signature: string
+                The signature of the sala bingo on the commitments.
+                
+        # Returns
+            true if the signature is valid, false otherwise.
+            
+        """
+        pass
+    
+    def send_opening(self):
+        """ 
+        The player sends its opening to the sala bingo.
+        # Returns
+            opening: string
+                The opening as (message, randomness) pair.
+        """
+        pass
+    
+    def recieve_openings(self, openings):
+        """ 
+        The player receives the openings from the sala bingo and computes
+        the final string.
+        # Arguments
+            openings: list
+                The list of openings.
+        """
+        pass
